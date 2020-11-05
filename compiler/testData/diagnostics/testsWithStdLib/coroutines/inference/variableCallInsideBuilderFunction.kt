d135e6ead31ad6a9befe429046a98d08a3dd07b3
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-11-05 14:25:55 +0300

    [TEST] Render regular debug infos in classic diagnostics handler

--- compiler/testData/diagnostics/testsWithStdLib/coroutines/inference/variableCallInsideBuilderFunction.kt
+++ compiler/testData/diagnostics/testsWithStdLib/coroutines/inference/variableCallInsideBuilderFunction.kt
@@ -4,17 +4,8 @@
 // !USE_EXPERIMENTAL: kotlin.RequiresOptIn
 // FULL_JDK
 
-import kotlin.experimental.ExperimentalTypeInference
-
-interface ProducerScope<E> {
-    fun yield(e: E)
-}
-
-@OptIn(ExperimentalTypeInference::class)
-fun <E> produce(@BuilderInference block: ProducerScope<E>.() -> Unit): ProducerScope<E> = TODO()
-
-fun <K> filter(e: K, predicate: (K) -> Boolean) =
-    produce {
-        predicate(e)
-        yield(42)
+fun foo(s: Any) {
+    if (s is String) {
+        s.length
     }
+}
